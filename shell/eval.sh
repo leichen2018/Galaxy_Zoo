edde1d30d9c7c0a06d24a6e0452c6abdfcd52ca6
Lei Chen <lchenthu@gmail.com>
2018-11-28 13:00:37 -0500

    sync, current best is 0.08620

--- shell/eval.sh
+++ shell/eval.sh
@@ -1,7 +1,7 @@
 set -x
-MODEL="groupy_360degree_001lr_250ep_0.5dp_0.9moment"
+MODEL="groupy_180degree_001lr_250ep_0.5dp_0.9moment"
 
 python3 eval.py  \
 --name ${MODEL}  \
---load  models/${MODEL}/model_246*  \
+--load  models/${MODEL}/model_229*  \
 #--crop
